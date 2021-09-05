41b743b86febf5ddb26a25e3da24dc3a6f473f5f
zefty <jaime.wu011@gmail.com>
2021-09-05 21:16:00 +1200

    feat: can change to multiple colours

--- main.py
+++ main.py
@@ -15,18 +15,16 @@ def main():
 
         # Get the size of the image
         width, height = im.size
-        print(width, height)
-        # Process every pixel
-        for x in range(width):
-            for y in range(height):
-                current_pixel_colour = im.getpixel((x, y))
-                if current_pixel_colour == make_tuple(config['colour_from']):
-                    im.putpixel((x, y), make_tuple(config['colour_to']))
-        
-        im.save(os.path.join(out, filename))
 
+        for colour_to in config['colour_to']:
+            # Process every pixel
+            for x in range(width):
+                for y in range(height):
+                    current_pixel_colour = im.getpixel((x, y))
+                    if current_pixel_colour == make_tuple(config['colour_from']):
+                        im.putpixel((x, y), make_tuple(colour_to))
 
-    
+            im.save(os.path.join(out, ' '.join([colour_to, filename])))
 
 if __name__ == "__main__":
     main()
