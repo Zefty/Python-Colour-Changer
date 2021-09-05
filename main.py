e2d061aa62b427d51e6216f8008ccacdd9603434
zefty <jaime.wu011@gmail.com>
2021-09-05 21:00:21 +1200

    simple python colour changer

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import os
+from PIL import Image
+from ast import literal_eval as make_tuple
+
+def main():
+    with open('config.json') as f:
+        config = json.load(f)
+        print(config)
+    
+    dir = r'./pics'
+    out = r'./out'
+    for filename in os.listdir(dir):
+        im = Image.open(os.path.join(dir, filename))
+
+        # Get the size of the image
+        width, height = im.size
+        print(width, height)
+        # Process every pixel
+        for x in range(width):
+            for y in range(height):
+                current_pixel_colour = im.getpixel((x, y))
+                if current_pixel_colour == make_tuple(config['colour_from']):
+                    im.putpixel((x, y), make_tuple(config['colour_to']))
+        
+        im.save(os.path.join(out, filename))
+
+
+    
+
+if __name__ == "__main__":
+    main()
